b9f10894dfd5ae9218970300ff951c161735b79a
adeelzhub <adeelijaz@hotmail.com>
2021-09-05 03:56:25 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+// const os = require("os")
+
+// const fs = require("fs")
+
+// let myFunctions = require("./myFunctions");
+
+const _ = require("lodash")
+
+
+
+// console.log(fun)
+
+
+// const alphabett = ["A","B","C","D","E","F"]
+// const numbers = ["1","2","3","4","5","6"]
+
+
+
+// const [x,,y,,,added="7"] = numbers;
+
+// console.log(x);
+// console.log(y)
+// console.log(added);
+
+// console.log(allButTheFirst);
+
+// const personOne = {
+//     name: "Adeel",
+//     age: 41,
+//     address: {
+//         city: "Manchester",
+//         area: "Moss side",
+//         postcode: "M16"
+//     }
+// }
+
+// const personTwo = {
+//     name: "Umer",
+//     age: 38,
+//     address: {
+//         city: "Manchester",
+//         area: "Rochdale",
+        
+//     }
+
+// }
+
+// const { name: firstname,age = "30", address :{area: hisArea}, employed = "no", address:{postcode = "Sk14"}} = personOne;
+
+// console.log(age)
+// console.log(hisArea)
+
+// console.log(firstname)
+// console.log(employed);
+
+// console.log(postcode);
+
+
+// const {age:per2Age,...rest} = personTwo
+
+// console.log(rest);
+
+
+// console.log(postcode)
+
+
+// const personThree = {...personTwo,...personOne}
+
+// const personThree = {name,...rest = personOne,...personTwo}
+
+
+// console.log(personThree)
+
+
+// const banana = require("./myFunctions")
+
+// const {add: fun, area:borough = "hyde" } = require("./myFunctions")
+
+// console.log(banana);
+
+// console.log(banana.add(5,6))
+
+// console.log(fun(8,9))
+
+// console.log(area)
+// console.log(borough)
+
+
+// console.log(os);
+
+// let userDetails = os.userInfo()
+
+// console.log(userDetails.toString());
+
+// console.log(os.userInfo())
+
+
+// console.log(myFunctions.add.toString())
+
+
+
+console.log(_.toString());
+
+
---
+++ myFunctions.js
@@ -0,0 +1,11 @@
+const add = (num1, num2) =>{
+    return num1 +num2
+}
+
+
+module.exports ={
+    add,
+    name: "Adeel",
+    desc: "This is my file",
+    // area: "Moss side"
+}
